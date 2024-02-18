de9ceb7afc7af7d56827fc679def3104e419c4f1
Faisal Malik <143418097+FaisalMalik4@users.noreply.github.com>
2024-02-18 17:50:29 +0500

    done class task 01

---
+++ index.js
@@ -0,0 +1,23 @@
+
+        //Welcome Message
+        // function displayWelcomeMessage(){
+        //     let name = prompt("please enter your name")
+        //     alert("Welcome "+name)
+        // }
+        // displayWelcomeMessage();
+
+        // Quote Generator
+        // function generateQuote(){
+        //     let quotes = ["Old is Gold", "Dream big", "Daal May Kuch Kala Hay", "Trust yourself", "Try again"];
+        //     let randomNumber = Math.round(Math.random()*(quotes.length-1));
+        //     alert(quotes[randomNumber])
+        // }
+        // generateQuote();
+
+        // Random Number Generator
+
+        function generateRandomNumber(){
+            let randomNumber = Math.round(Math.random()*100);
+            alert(randomNumber)
+        }
+        generateRandomNumber();
